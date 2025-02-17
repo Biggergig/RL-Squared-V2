e66f60f718c31a4442dbda5d583f6abbb9b250cf
Anshuman Dash <anshuman@ucsb.edu>
2025-02-16 21:33:46 -0800

    More finetuning + utils for loading checkpoints

--- anshuman/metricLogger.py
+++ anshuman/metricLogger.py
@@ -10,28 +10,28 @@ class MyMetricLogger(MetricsLogger):
     def _collect_metrics(self, game_state: GameState) -> list:
         return [
             game_state.orange_score + game_state.blue_score,
-            1.0 if game_state.players[0].ball_touched else 0.0,
-            1.0 if game_state.players[1].ball_touched else 0.0,
             sum(
                 self.distToBallReward.get_reward(player, game_state, None)
                 for player in game_state.players
             )
             / len(game_state.players),
+            sum(player.ball_touched for player in game_state.players)
+            / len(game_state.players),
         ]
 
     def _report_metrics(self, collected_metrics, wandb_run, cumulative_timesteps):
-        avgs = [0, 0, 0, 0]
+        avgs = [0, 0, 0]
         for metric_array in collected_metrics:
             for i in range(len(avgs)):
                 avgs[i] += metric_array[i]
         for i in range(len(avgs)):
             avgs[i] /= len(collected_metrics)
+
         report = {
             "1. Average Total Score": avgs[0],
-            "2. Player 0 ball touch averages": avgs[1],
-            "3. Player 1 ball touch averages": avgs[2],
-            "4. Average total distance to ball": avgs[3],
-            "5. Cumulative Timesteps": cumulative_timesteps,
-            "6. Num Metrics": len(collected_metrics),
+            "2. Average total distance to ball": avgs[1],
+            "3. Average times ball touched": avgs[2],
+            "4. Cumulative Timesteps": cumulative_timesteps,
+            "5. Num Metrics": len(collected_metrics),
         }
         wandb_run.log(report)
--- anshuman/train.py
+++ anshuman/train.py
@@ -1,4 +1,6 @@
 import numpy as np
+import os
+from time import time
 
 from metricLogger import MyMetricLogger
 from rewards import *
@@ -59,7 +61,9 @@ def build_rocketsim_env():
         reward_fn=reward_fn,
         obs_builder=obs_builder,
         action_parser=action_parser,
-        state_setter=RandomState(True, True),  # Randomized speeds for cars + ball
+        state_setter=RandomState(
+            True, True, False
+        ),  # Randomized speeds for cars + ball, and can be in the air
     )
 
     return env
@@ -69,11 +73,15 @@ if __name__ == "__main__":
     from rlgym_ppo import Learner
     from sys import argv
 
+    argv = [s.lower() for s in argv]
+
     DEBUG = "debug" in argv or "dry" in argv
     if DEBUG:
         print("IN DEBUG MODE")
 
     NEWRUN = "new" in argv
+    if NEWRUN:
+        print("Creating new run")
 
     metrics_logger = MyMetricLogger()
 
@@ -88,10 +96,22 @@ if __name__ == "__main__":
     PPO_ENT_COEF = 0.01
     NET_LR = 2e-4
     SAVE_EVERY_TS = 250_000
+    CHECKPOINT_SAVE_FOLDER = "data/checkpoints"
+    try:
+        CHECKPOINT_LOAD_DIR = "data/checkpoints/" + str(
+            max(os.listdir("data/checkpoints/"), key=lambda d: int(d.split("-")[-1]))
+        )
+        CHECKPOINT_LOAD_DIR += "/" + max(
+            os.listdir(CHECKPOINT_LOAD_DIR), key=lambda d: int(d.split("-")[-1])
+        )
+    except (ValueError, FileNotFoundError):
+        CHECKPOINT_LOAD_DIR = None
 
     if DEBUG:
         N_PROC = 1
         SAVE_EVERY_TS = 1_000_000_000_000
+        CHECKPOINT_SAVE_FOLDER = "data/debug/checkpoints"
+        CHECKPOINT_LOAD_DIR = None
 
     learner = Learner(
         build_rocketsim_env,
@@ -110,15 +130,16 @@ if __name__ == "__main__":
         critic_lr=NET_LR,
         policy_lr=NET_LR,
         log_to_wandb=not DEBUG,
-        checkpoints_save_folder=None,
+        wandb_run_name="PPO-" + str(int(time()) % 1_000_000),
+        checkpoints_save_folder=CHECKPOINT_SAVE_FOLDER,
         save_every_ts=SAVE_EVERY_TS,
         n_checkpoints_to_keep=100_000,
         metrics_logger=metrics_logger,
+        checkpoint_load_folder=CHECKPOINT_LOAD_DIR,
+        load_wandb=not DEBUG,
         #   min_inference_size=min_inference_size,
         #   standardize_returns=True,
         #   standardize_obs=False,
-        checkpoint_load_folder="latest" if not (DEBUG or NEWRUN) else None,
-        load_wandb=True,
     )
 
     with open(__file__, "r") as f:
