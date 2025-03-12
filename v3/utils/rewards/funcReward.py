48ff83afd102be8f530e42c9b24343b21f69e1a6
Anshuman Dash <anshuman@ucsb.edu>
2025-03-11 21:52:23 -0700

    timescale function

--- v3/utils/rewards/funcReward.py
+++ v3/utils/rewards/funcReward.py
@@ -29,3 +29,32 @@ class FnReward(RewardFunction):
         rew = self.fn.get_final_reward(*args, **kwargs)
         self.ts += 1
         return self.scale(rew, self.ts * self.n_proc)
+
+
+class TimeReward(RewardFunction):
+    def __init__(
+        self,
+        reward_fn: RewardFunction,
+        time_fn,
+        n_proc: int = 1,
+    ):
+        super().__init__()
+        self.fn = reward_fn
+        self.time_fn = time_fn
+        self.ts = 0
+        self.n_proc = n_proc
+
+    # Called when the game resets (i.e. after a goal is scored)
+    def reset(self, initial_state: GameState):
+        self.fn.reset(initial_state)
+
+    # Get the reward for a specific player, at the current state
+    def get_reward(self, *args, **kwargs) -> float:
+        rew = self.fn.get_reward(*args, **kwargs)
+        self.ts += 1
+        return rew * self.time_fn(self.ts * self.n_proc)
+
+    def get_final_reward(self, *args, **kwargs) -> float:
+        rew = self.fn.get_final_reward(*args, **kwargs)
+        self.ts += 1
+        return rew * self.time_fn(self.ts * self.n_proc)
